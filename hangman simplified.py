a68569cba5b88deb82a9b0744c09633e2069f6d4
matthowe <mhowe@headingtonschool.com>
2017-02-01 10:17:39 +0000

    Cleaned up code a little - some work still needed

--- hangman simplified.py
+++ hangman simplified.py
@@ -1,90 +1,62 @@
 import random
 
 def getWord():
+   """Get a word from the dictionary text file - done once. Words are held in list for all later games""" 
    f = open("wordsEn.txt")
    words = []
    for word in f:
       if len(word) > 2: #only add words of 3 characters or more
          words.append(word.rstrip('\n')) #strip the end of line from each word
-   randomWord = random.choice(words)
-   return randomWord
+   return words
 
-def guessLetter(word): 
-   usedLetters = []
-   miss = 0
-   displayWord = []
-   for i in range(len(word)):
-      displayWord.append("*")
-   keepPlaying = True
+def guessLetter(word):
+    """Main guessing game"""
+    usedLetters = [] #list of used letters
+    miss = 0 #counts the number of missed letters
+    displayWord = [] #displays the word being guessed
+    for i in range(len(word)):
+       displayWord.append("*") #initially displays the word's letters as *s
    
-   while keepPlaying == True:
-      for i in displayWord:
-         print(i,end="")
-      print()
-      print("Used Letters = ",end="")
-      usedLetters.sort()
-      for i in usedLetters:
-         print(i,end="")
-      print()
-      print("Misses = ",miss)
-      guess = input("Guess a Letter? ",)
+    while miss != 8: #game will loop until number of misses is 8 
+       for i in displayWord:
+          print(i,end="")
+       print()
+       print("Used Letters = ",end="")
+       usedLetters.sort()
+       for i in usedLetters:
+          print(i,end="")
+       print()
+       print("Misses = ",miss)
+       guess = input("Guess a Letter? ",)
 
-      if guess in usedLetters:
-         print("You have used that letter before")
-         return(keepPlaying,displayWord, miss, usedLetters)
-      
-      charPosition = 0
+       found = False   
 
+       if guess in usedLetters:
+          print("You have used that letter before")
+              
+       elif found == False:
+          miss = miss + 1
+          usedLetters.append(guess)   
+       charPosition = 0
 
-      
-      found = False
-      for i in word:
-         if guess == i:
-            displayWord[charPosition] = i
-            found = True
-         charPosition = charPosition + 1
-      if found == False:
-         miss = miss + 1
-         usedLetters.append(guess)
+       for i in word:
+          if guess == i:
+             displayWord[charPosition] = i
+             found = True
+          charPosition = charPosition + 1
+
+       # Have you guessed the word correctly
+       displayWordStr = "" #convert the displayWord list into a string
+       for i in displayWord:
+          displayWordStr = displayWordStr + i
          
+       if miss == 8:
+          print("YOU HAVE LOST")
+         
+       if displayWordStr == word: #see if the displayWordStr matches the word
+          print("YOU HAVE WON")
+  
 
-      # Have you guessed the word correctly
-      displayWordStr = "" #convert the displayWord list into a string
-      for i in displayWord:
-         displayWordStr = displayWordStr + i
-
-      if displayWordStr == word: #see if the displayWordStr matches the word
-         print("YOU HAVE WON")
-         keepPlaying = False
-
-      if miss == 8:
-         print("YOU HAVE LOST")
-         keepPlaying = False
-
-      
-      
-   
-# generates our word to guess
-word = getWord()
-print(word)
-
-# generates the guess word - starts all blanks
-#displayWord = []  #displayWord is a list as we need to change the characters
-                  #from blanks to guessed letters
-                  #and strings are immuatable
-
-
-#usedLetters = []
-#keepPlaying = True
-#miss = 0
-
-guessLetter(word)
-
-
-#while keepPlaying == True:
-#   keepPlaying, displayWord, miss, usedLetters = guessLetter(word, displayWord, keepPlaying, miss, usedLetters)
-
-
-
-
-   
+words = getWord() # gets words
+randomWord = random.choice(words) #chooses a random word to guess
+guessLetter(randomWord)
